8a1f8dda20ced90459447807b813a942f02e99aa
kevin <khan1782@gmail.com>
2017-02-25 13:41:59 -0600

    seperated controller and router code, added websocket class for testing

--- Controllers/controller.js
+++ Controllers/controller.js
@@ -1,45 +1,37 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-
   //identify starfield div container
   var background = document.getElementById('background');
+
   //initialize new starfield beep beep boop
   var starfield = new Starfield();
+
   //run initialize on initialized starfield
   starfield.initialize(background);
+
   //begin starfield loop
   starfield.start();
 
 
-//-----------------Server Side----------------------
-  var game = new Game()
-  game.players.push(new Player)
-  game.players[0].ship = new Ship
-  game.gameLoop()
-//--------------------------------------------------
-
+//---------------will receive from server---------
   var currentSnapshot = function() {
     return JSON.parse(game.snapshot())
   }
+//------------------------------------------------
 
   //identify game canvas
   var canvas = document.getElementById("gameCanvas")
+
   //initialize renderer machinery *wa wa wa wa*
   render = new Renderer(canvas, currentSnapshot())
+
   //set game bounds
   render.initialize()
-  //start of the game loop (running draw at 50fps)
-  //update the assets per loop
 
-
-
-  //snapshot is captured
-
-
-  // render.gameLoop(currentSnapshot());
+  //run gameloop on currentSnapshot
   render.gameLoop(currentSnapshot);
+
   //add listeners for key strokes for initialized game
   keyStrokeListeners(game)
- 
 
 
 })
@@ -59,12 +51,15 @@ function keyStrokeListeners(game) {
       keys.up = true;
       // send json to server controller
       //SEND THIS!!! JSON.stringify(keys)
+      websock.object = keys
+
     }
     //listening for down
     if(event.keyCode === 40 && keys.down === false ) {
       keys.down = true;
       // send json to server controller
        //SEND THIS!!! JSON.stringify(keys)
+       websock.object = keys
     }
 
     //listening for left
@@ -72,6 +67,7 @@ function keyStrokeListeners(game) {
       keys.left = true;
       // send json to server controller
        //SEND THIS!!! JSON.stringify(keys)
+       websock.object = keys
     }
 
     // listening for right
@@ -79,6 +75,7 @@ function keyStrokeListeners(game) {
       keys.right = true;
       // send json to server controller
       //SEND THIS!!! JSON.stringify(keys)
+      websock.object = keys
     }
   });
 
@@ -87,22 +84,27 @@ function keyStrokeListeners(game) {
     if(event.keyCode === 38) {
       keys.up = false;
        //SEND THIS!!! JSON.stringify(keys)
+       websock.object = keys
     }
     if(event.keyCode === 40) {
       keys.down = false;
        //SEND THIS!!! JSON.stringify(keys)
+       websock.object = keys
     }
     if(event.keyCode === 37) {
       keys.left = false;
        //SEND THIS!!! JSON.stringify(keys)
+       websock.object = keys
     }
     if(event.keyCode === 39) {
       keys.right = false;
        //SEND THIS!!! JSON.stringify(keys)
+       websock.object = keys
     }
     if(event.keyCode === 32) {
       // send a missle json that is stamped with a user id
       //SEND THIS!!!! "fire"
+      websock.object = {fire:"pew"}
     }
   });
 }
---
+++ Controllers/router.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+
+  game = new Game()
+  game.players.push(new Player)
+  game.players[0].ship = new Ship
+  game.gameLoop();
+  websock = new Websocket
+});
+
+
+
+
+
+
+
+
+
+function Websocket(){
+}
